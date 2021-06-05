a9908e640b2553075fcdcfb55470164f452b1be1
Your Name <you@example.com>
2021-06-04 21:20:14 -0600

    second commit

---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-6xl">Exchange v1.0.0</h1>
+    <p class="text-gray-600 text-xl text-center">
+      Este Proyecto es una plataforma que te servirá para obtener los valores en tiempo real de las cryptomonedas mas
+      importantes a traves de la API REST de Coincap.
+      <br>
+      <br />Desarrollado para la clase de programacion III, en la universidad Mariano Galvez.
+      <br>
+      <br>
+      <br/> By Daniel Riveiro (☞ﾟヮﾟ)☞
+    </p>
+
+    <router-link to="/" class="mt-5 text-xl text-green-600 hover:underline"
+      >Volver a la pagina de Inicio</router-link
+    >
+  </div>
+</template>
